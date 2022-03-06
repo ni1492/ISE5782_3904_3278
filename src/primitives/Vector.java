bc8b63f21b3fd01f78062ac4889cdff21e534d0b
ni1492 <nilan@g.jct.ac.il>
2022-03-06 21:04:10 +0200

    final changes in primitives

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -45,7 +45,47 @@ public class Vector extends Point
 		{
 			return new Vector(xyz.scale(scaler));
 		}
-		cha
+		catch(IllegalArgumentException Execption)
+		{
+			throw new IllegalArgumentException("Vector(0,0,0) is not allowed");	
+		}
 	}
 	
+	public double dotProduct (Vector other)
+	{
+		double d1=other.xyz.d1;
+		double d2=other.xyz.d2;
+		double d3=other.xyz.d3;
+
+		return (xyz.d1*d1+xyz.d2*d2+xyz.d3*d3);
+	}
+	
+	public Vector crossProduct(Vector other)
+	{
+		double x1 = xyz.d1;
+        double x2 = other.xyz.d1;
+        double y1 = xyz.d2;
+        double y2 = other.xyz.d2;
+        double z1 = xyz.d3;
+        double z2 = other.xyz.d3;
+        double i = y1 * z2 - z1 * y2;
+        double j = z1 * x2 - x1 * z2;
+        double k = x1 * y2 - y1 * x2;
+        return new Vector(i, j, k);
+	}
+	
+	public double lengthSquared()
+	{
+		return (xyz.d1)*(xyz.d1)+(xyz.d2)*(xyz.d2)+(xyz.d3)*(xyz.d3);
+	}
+	
+	public double length()
+	{
+		return Math.sqrt(this.lengthSquared());
+	}
+	
+	public Vector normalize()
+	{
+		return new Vector(this.xyz.reduce(this.length()));
+	}
 }
