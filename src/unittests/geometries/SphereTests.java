945d5f55550ac84c7d15a27c024273467432c5fc
ni1492 <nilan@g.jct.ac.il>
2022-03-30 17:48:55 +0300

    fix SphereTests

--- src/unittests/geometries/SphereTests.java
+++ src/unittests/geometries/SphereTests.java
@@ -48,7 +48,7 @@ class SphereTests
         List<Point> result = sphere.findIntersections(new Ray(new Point(-1, 0, 0),
                                                                 new Vector(3, 1, 0)));
         assertEquals(2, result.size(), "Wrong number of points");        
-        if (result.get(0).getX() > result.get(1).getX())
+        if (result.get(0).getXyz().getX() > result.get(1).getXyz().getX())
         {
             result = List.of(result.get(1), result.get(0));
         }
