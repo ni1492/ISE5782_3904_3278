26cc79b8e14dc5d48ab15cb49380131687a671ae
USER <USER@LAPTOP-IN86FDFV>
2022-04-25 14:28:39 +0300

    comments and ignore

--- src/geometries/Intersectable.java
+++ src/geometries/Intersectable.java
@@ -6,9 +6,13 @@ import primitives.*;
 public interface Intersectable 
 {
 	/**
+	 * Deprecated function! no longer in use.
 	 * the func gets ray and returns list of intersections with the geometry
 	 * @param ray type- the ray that intersects with the shape 
 	 * @return List of intersections points with the shape
 	 */
 	public List<Point> findIntersections(Ray ray);
 }
+
+//protected helper
+//PDS= public!!!!(in material class)
